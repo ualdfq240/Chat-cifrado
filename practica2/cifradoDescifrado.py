ca64bdc98817580e0199cbdcb22ef9e153a48b11
David Fernández Quiñones <dfq240@inlumine.ual.es>
2024-10-17 19:14:21 +0200

    Menu

--- practica2/cifradoDescifrado.py
+++ practica2/cifradoDescifrado.py
@@ -1,45 +1,56 @@
 
 
 
+
 # Función para seleccionar el tamaño de la clave
 def elegir_tamaño_clave():
-    print("Seleccione el tamaño de la clave AES:")
-    print("1. 128 bits (16 bytes)")
-    print("2. 192 bits (24 bytes)")
-    print("3. 256 bits (32 bytes)")
+    print()
     
-    opcion = input("Ingrese su opción (1/2/3): ")
-    
-    if opcion == '1':
-        return 16  # 128 bits
-    elif opcion == '2':
-        return 24  # 192 bits
-    elif opcion == '3':
-        return 32  # 256 bits
-    else:
-        print("Opción no válida. Seleccionando 128 bits por defecto.")
-        return 16
+    opcion = int(input("""  
+                Seleccione el tamaño de la clave AES:
+                1. 128 bits (16 bytes)
+                2. 192 bits (24 bytes)
+                3. 256 bits (32 bytes)
+                Ingrese su opción (1/2/3): """))
+    while True:
+        if opcion == 1:
+            return 16  # 128 bits
+        elif opcion == 2:
+            return 24  # 192 bits
+        elif opcion == 3:
+            return 32  # 256 bits
+        opcion = int(input("""  
+                Seleccione el tamaño de la clave AES:
+                1. 128 bits (16 bytes)
+                2. 192 bits (24 bytes)
+                3. 256 bits (32 bytes)
+                Ingrese su opción (1/2/3): """))
 
 # Función para seleccionar el modo de operación
 def elegir_modo_operacion():
-    print("\nSeleccione el modo de operación AES:")
-    print("1. CBC (Cipher Block Chaining)")
-    print("2. ECB (Electronic Codebook)")
-    print("3. OFB (Output Feedback Mode)")
+    opcion = int(input("""  
+                Seleccione el modo de operación AES:
+                1. CBC (Cipher Block Chaining)
+                2. ECB (Electronic Codebook)
+                3. OFB (Output Feedback Mode)
+                Ingrese su opción (1/2/3): """))
+    while True: 
+        if opcion == 1:
+            return 'MODE_CBC'
+        elif opcion == 2:
+            return 'MODE_ECB'
+        elif opcion == 3:
+            return 'MODE_OFB'
+        opcion = int(input("""  
+                Seleccione el modo de operación AES:
+                1. CBC (Cipher Block Chaining)
+                2. ECB (Electronic Codebook)
+                3. OFB (Output Feedback Mode)
+                Ingrese su opción (1/2/3): """))
+bits = elegir_tamaño_clave()
+criptografia = elegir_modo_operacion()
 
-
-    opcion = input("Ingrese su opción (1/2/3): ")
-    
-    if opcion == '1':
-        return AES.MODE_CBC
-    elif opcion == '2':
-        return AES.MODE_ECB
-    elif opcion == '3':
-        return AES.MODE_OFB
-    else:
-        print("Opción no válida. Seleccionando CBC por defecto.")
-        return AES.MODE_CBC
-
-
-
+print(f"""
+Numero de bits de la clave: {bits}
+Modo de operación: {criptografia}""")   
 # Función para cifrar un archivo
